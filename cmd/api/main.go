2491cf4ae8cf2f5851b2b3dc3e7b25a07d1d4409
Kamil-Budzik <mixboksu@gmail.com>
2026-02-06 13:42:21 +0100

    Add postgres DB connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/kamil-budzik/csv-processor/internal/api"
 	"github.com/kamil-budzik/csv-processor/internal/config"
+	"github.com/kamil-budzik/csv-processor/internal/db"
 )
 
 func main() {
 	cfg := config.LoadConfig()
+
+	// DB Setup
+	db.Connect(cfg)
+	db.DB.SetMaxOpenConns(25)
+	db.DB.SetMaxIdleConns(25)
+	db.DB.SetConnMaxLifetime(time.Hour)
+	defer db.DB.Close()
 
 	server := api.NewServer(cfg.Port)
 
---
+++ internal/db/db.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/kamil-budzik/csv-processor/internal/config"
+	_ "github.com/lib/pq" // Postgres driver
+)
+
+var DB *sql.DB
+
+func Connect(cfg config.Config) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName,
+	)
+
+	var err error
+	DB, err = sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	log.Println("Successfully connected to PostgreSQL")
+}
